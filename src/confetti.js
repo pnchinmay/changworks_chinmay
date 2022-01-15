d0dd9baa834c46c9cf837017b3e173749faadbdc
pnchinmay <pnchinmay9@gmail.com>
2022-01-16 03:06:27 +0530

    Style: more random confetti

--- src/confetti.js
+++ src/confetti.js
@@ -1,21 +1,21 @@
 import confetti from "canvas-confetti";
 
 const myConfetti = () => {
-    var duration = 2 * 1000;
+    var duration = 2 * 10;
     var end = Date.now() + duration;
-    function randomxPosition(num) {
-        let x = Math.random() * (num-1) + num;
-        console.log("x = " + x);
+    console.log(end);
+    function randomxPosition(min,max) {
+        let x = Math.random() * (max-min) + min;
         return x;
     }
     function randomyPosition() {
-        return Math.random() - 0.2;
+        return Math.random() - 0.1;
     }
     var defaults = { startVelocity: 40, spread: 360, particleCount: 20, gravity: 0.8};
     confetti(
         Object.assign({}, defaults, {
         origin: {
-            x: randomxPosition(0.4),
+            x: randomxPosition(0.1,0.4),
             y: randomyPosition(),
         },
         })
@@ -23,7 +23,7 @@ const myConfetti = () => {
     confetti(
         Object.assign({}, defaults, {
         origin: {
-            x: randomxPosition(0.6),
+            x: randomxPosition(0.6,0.9),
             y: randomyPosition(),
         },
         })
